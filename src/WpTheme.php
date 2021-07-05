167441e01145f6cdfa4094f72f88df8786e27715
Phuc Nguyen <phucnguyen@Phucs-iMac-Pro.local>
2021-07-05 17:09:12 +0700

    Add sample theme

---
+++ src/WpTheme.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phucnguyen
+ * Date: 5/05/21
+ * Time: 17:03 PM
+ */
+
+namespace Yivic\Wp\YivicSample2021;
+
+
+class WpTheme extends \Yivic\Wp\YivicBase\Component\WpTheme {
+
+}
